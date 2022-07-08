88dc5402bf631c0c7c9c234d62f0afeae1d5e2ee
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2022-07-08 10:40:42 -0400

    main

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-const scrollTop = document.querySelector(".scrolltop");
 const headerEl = document.querySelector(".header-wrapper");
 
-
-window.addEventListener("scroll", function () {
-    let yAxis = window.scrollY;
-    if (yAxis >= 100) {
-      scrollTop.classList.remove("hidden");
-    } else {
-      scrollTop.classList.add("hidden");
-    }
-  });
   
   window.addEventListener("scroll", function () {
     let yAxis = window.scrollY;
